ce7e5b88f6d12ecaacf29e7072a240ec778c1bb1
pramudya-27 <dio2431058@itpln.ac.id>
2026-03-26 18:25:17 +0700

    add fe service

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -3,11 +3,11 @@
 # Membangun Docker image dari Dockerfile di direktori saat ini dengan nama tag 'karsajobs-ui:latest'
 docker build -t karsajobs-ui:latest .
 
-# Menambahkan tag pada Docker image agar sesuai dengan username dan repositori Docker Hub
-docker tag karsajobs-ui:latest dio713/karsajobs-ui:latest
+# Menambahkan tag pada Docker image agar sesuai dengan registry GitHub Packages (GHCR)
+docker tag karsajobs-ui:latest ghcr.io/pramudya-27/karsajobs-ui:latest
 
-# Login ke Docker Hub dan password yang diambil dari environment variable PASSWORD_DOCKER_HUB
-echo $PASSWORD_DOCKER_HUB | docker login -u dio713 --password-stdin
+# Login ke GitHub Packages (ghcr.io) menggunakan environment variable CR_PAT (Personal Access Token)
+echo $CR_PAT | docker login ghcr.io -u pramudya-27 --password-stdin
 
-# Mengunggah (push) Docker image ke repository Docker Hub
-docker push dio713/karsajobs-ui:latest
+# Mengunggah (push) Docker image ke repository GitHub Packages
+docker push ghcr.io/pramudya-27/karsajobs-ui:latest
